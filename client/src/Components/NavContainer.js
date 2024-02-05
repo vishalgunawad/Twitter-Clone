94f71259e1b497623431f5df48f5639b1ee0b6ea
Satellite-system <adarshurmaliya@gmail.com>
2024-02-05 22:06:43 +0530

    Add Navbar Completed

---
+++ client/src/Components/NavContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavContainer = ({ title, icon, selected }) => {
+  console.log(title, " => ", selected);
+  return (
+    <div className="flex pl-5">
+      <div className="flex box-container rounded-full py-2 px-3 gap-3">
+        <img src={icon} alt={title} className={`${selected ? "w-9" : "w-8"}`} />
+        <p className={`${selected ? "font-bold text-2xl" : "text-xl"}`}>
+          {title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NavContainer;
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,22 +1,57 @@
-import React from "react";
-import logo from "./../Assets/svg0.svg";
+import React, { useState } from "react";
+import logo from "./../Assets/logo.svg";
+import exploreIcon from "./../Assets/lens.svg";
+import homeIcon from "./../Assets/home.svg";
+import notificationIcon from "./../Assets/bell.svg";
+import msgIcon from "./../Assets/msg.svg";
+import profileIcon from "./../Assets/profile.svg";
 import { NavLink } from "react-router-dom";
+import NavContainer from "./NavContainer";
 
 const Navbar = () => {
+  const [selectedTag, setSelectedTag] = useState(0);
+
   return (
-    <header className="flex flex-col">
-      {/* Navbar */}
-      {/* <p className="text-3xl">Yhis is #XL</p> */}
-      <NavLink to="/home">
-        <img src={logo} alt="X" className="h-12" />
+    <header className="flex flex-col w-2/5 px-5 pt-5">
+      <NavLink to="/home" onClick={() => setSelectedTag(0)}>
+        <img src={logo} alt="X" className="h-12 ml-6" />
       </NavLink>
-      <nav className="flex flex-col">
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/">Explore</NavLink>
-        <NavLink to="/">Notifications</NavLink>
-        <NavLink to="/">Messages</NavLink>
-        <NavLink to="/">Profile</NavLink>
-        <NavLink to="/">Messages</NavLink>
+      <nav className="flex flex-col gap-5 mt-5  justify-center">
+        <NavLink to="/home" onClick={() => setSelectedTag(0)}>
+          <NavContainer
+            title="Home"
+            icon={homeIcon}
+            selected={selectedTag === 0}
+          />
+        </NavLink>
+        <NavLink to="/" onClick={() => setSelectedTag(1)}>
+          <NavContainer
+            title="Explore"
+            icon={exploreIcon}
+            selected={selectedTag === 1}
+          />
+        </NavLink>
+        <NavLink to="/" onClick={() => setSelectedTag(2)}>
+          <NavContainer
+            title="Notifications"
+            icon={notificationIcon}
+            selected={selectedTag === 2}
+          />
+        </NavLink>
+        <NavLink to="/" onClick={() => setSelectedTag(3)}>
+          <NavContainer
+            title="Messages"
+            icon={msgIcon}
+            selected={selectedTag === 3}
+          />
+        </NavLink>
+        <NavLink to="/" onClick={() => setSelectedTag(4)}>
+          <NavContainer
+            title="Profile"
+            icon={profileIcon}
+            selected={selectedTag === 4}
+          />
+        </NavLink>
       </nav>
     </header>
   );
