e387e30228ed6eca563116472f10ba483665e11d
Satellite-system <adarshurmaliya@gmail.com>
2024-02-06 19:50:49 +0530

    Update Css for Navbar

--- client/src/Components/NavContainer.js
+++ client/src/Components/NavContainer.js
@@ -4,7 +4,7 @@ const NavContainer = ({ title, icon, selected }) => {
   console.log(title, " => ", selected);
   return (
     <div className="flex pl-5">
-      <div className="flex box-container rounded-full py-2 px-4 gap-3">
+      <div className="flex hover:bg-gray-200 rounded-full py-2 px-4 gap-3">
         <img src={icon} alt={title} className={`${selected ? "w-9" : "w-8"}`} />
         <p className={`${selected ? "font-bold text-2xl" : "text-xl"}`}>
           {title}
